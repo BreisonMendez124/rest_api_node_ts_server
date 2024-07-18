81137d500af2c2fbd7e824a9eec7a1fb7f05dfbc
BreisonMendezServi <breison.mendez@servinformacion.com>
2024-07-18 18:49:19 -0500

    Crear conexión a la base de datos

--- src/server.ts
+++ src/server.ts
@@ -1,7 +1,21 @@
 import express from 'express'
 import router from './router';
+import db from './config/db';
 
+
+//Conecta a base de datos
+async function connectDB(){
+    try{ 
+        await db.authenticate();
+        db.sync();
+        console.log('Conexión exitosa a la base de datos')
+    }catch( error ){ 
+        console.log("🚀 ~ connectDB ~ error:", error)
+        console.log('Hubo un error al conectar a la base de datos')
+    }
+}
 const server = express();
 server.use('/api/products' , router );
+connectDB()
 
 export default server
