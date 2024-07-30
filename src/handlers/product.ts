2e82445c62c2ccec11ba88cd6c7ad2d448557bbe
BreisonMendezServi <breison.mendez@servinformacion.com>
2024-07-29 20:14:45 -0500

    Finalizacion del metodo post - Creacion Prod. Se crea el middleware para validar los errores en la peticion
    Se valida que los datos no sean vacios para poder
    guardarlos correctamente
    
    Creacion de metodo de guardado de productos a la
    base de datos

--- src/handlers/product.ts
+++ src/handlers/product.ts
@@ -2,10 +2,26 @@ import { Request, Response } from "express"
 import Product from "../models/Product.model"
 
 export const createProduct = async( req : Request , res: Response ) => { 
-    const product = await Product.create( req.body );
-    res.json({ 
-        ok: true,
-        msg:'Producto guardado con exito!',
-        data: product 
-    })
+
+    //Validacion de campos en una funcion asincrona
+    // await check('name')
+    //         .notEmpty().withMessage('El nombre del Producto no puede ir vacio')
+    //         .run( req )
+    // await check('price')
+    //         .isNumeric().withMessage('Valor no valido')
+    //         .notEmpty().withMessage('El precio del Producto no puede ir vacio')
+    //         .custom( value => value > 0 ).withMessage('Precio no valido')
+    //         .run( req )
+
+    //Creacion del producto
+    try {
+        const product = await Product.create( req.body );
+        res.json({ 
+            ok: true,
+            msg:'Producto guardado con exito!',
+            data: product 
+        })
+    } catch (error) {
+        console.log("🚀 ~ createProduct ~ error:", error)
+    }
 }
--- src/router.ts
+++ src/router.ts
@@ -1,5 +1,7 @@
 import { Router } from 'express'
 import { createProduct } from './handlers/product';
+import { body } from 'express-validator';
+import { handleInputErrors } from './middleware';
 
 const router = Router();
 
@@ -8,6 +10,16 @@ router.get("/" , ( req , res ) => {
     res.json( 'Desde GET' )
 });
 
-router.post("/" , createProduct );
+router.post("/" , 
+     //Validacion de campos en el router
+     body('name')
+     .notEmpty().withMessage('El nombre del Producto no puede ir vacio'),
+     body('price')
+     .isNumeric().withMessage('Valor no valido')
+     .notEmpty().withMessage('El precio del Producto no puede ir vacio')
+     .custom( value => value > 0 ).withMessage('Precio no valido'),     
+    handleInputErrors,
+    createProduct 
+);
 
 export default router
