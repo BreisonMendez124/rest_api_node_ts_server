e202ca35727fef7ebdfbed44eadf0a14c22c369d
BreisonMendezServi <breison.mendez@servinformacion.com>
2024-09-10 18:36:56 -0500

    Finalizacion

--- src/server.ts
+++ src/server.ts
@@ -4,6 +4,7 @@ import swaggerUi from 'swagger-ui-express'
 import db from './config/db';
 import colors from 'colors'
 import cors , { CorsOptions }  from 'cors'
+import morgan from 'morgan'
 import swaggerSpec from './config/swagger';
 
 
@@ -22,15 +23,13 @@ connectDB()
 //Instanciar express
 const server = express();
 
-//Permitir conexiones
-
+//Permitir conexiones a origenes cruzados
 const corsOptions: CorsOptions = { 
     origin: ( origin , callback) => { 
         if( origin === process.env.FRONTEND_URL ){
             callback( null , true )
         }else { callback( new Error('Error de CORS')) }
     }
-    
 }
 
 server.use( cors( corsOptions ) );
@@ -38,6 +37,7 @@ server.use( cors( corsOptions ) );
 
 //Leer datos de formularios
 server.use( express.json( )) 
+server.use( morgan('dev') )
 server.use('/api/products' , router );
 
 server.use('/docs' , swaggerUi.serve  , swaggerUi.setup( swaggerSpec ))
